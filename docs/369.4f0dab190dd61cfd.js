65ca25cc03454d34af01fd3ea09f5a8467ad45f3
TatyanaGTV <tatyana_golikova@mail.ru>
2026-02-10 13:29:22 +0300

    added docs folder

--- docs/369.4f0dab190dd61cfd.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkDiapole=self.webpackChunkDiapole||[]).push([[369],{369:(d,l,o)=>{o.r(l),o.d(l,{UserModule:()=>s});var n=o(895),u=o(256);let s=(()=>{class e{}return e.\u0275fac=function(r){return new(r||e)},e.\u0275mod=u.oAB({type:e}),e.\u0275inj=u.cJS({imports:[n.ez]}),e})()}}]);
